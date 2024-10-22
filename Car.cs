a6f964b916ae003ac40a72b410766b9fe1bffc32
wigscs <174610664+wigscs@users.noreply.github.com>
2024-10-22 14:01:43 +0100

    Initial Commit

---
+++ Car.cs
@@ -0,0 +1,43 @@
+using System.Globalization;
+
+namespace App;
+class Car
+{
+    private readonly string _make;
+    private readonly string _model;
+    private readonly int _year;
+    private string _registration;
+    private double _value;
+
+    public Car(string make, string model, int year, string registration, double value)
+    {
+        _make = make;
+        _model = model;
+        _year = year;
+        _registration = registration;
+        _value = value;
+    }
+
+    public double GetValue()
+    {
+        return _value;
+    }
+    public string GetGBValue()
+    {
+        return _value.ToString("C", CultureInfo.CreateSpecificCulture("en-gb"));
+    }
+
+    public int GetYear()
+    {
+        return _year;
+    }
+
+    public override string ToString()
+    {
+        return  "Make: " + _make + Environment.NewLine +
+                "Model: " + _model + Environment.NewLine +
+                "Year: " + GetYear() + Environment.NewLine +
+                "Registration: " + _registration + Environment.NewLine +
+                "Value: " + GetGBValue();
+    }
+}
---
+++ CarTest.cs
@@ -0,0 +1,11 @@
+namespace App;
+
+class CarTest
+{
+    public static void Run()
+    {
+        Car car = new Car("Tesla", "Model S", 2023, "AY23 WMJ", 46995);
+
+        System.Console.WriteLine(car);
+    }
+}
